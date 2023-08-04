d823d4dc10edc12057a4ea5536291842777dc966
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-04 18:58:56 -0400

    Starting a DoublyLinkedList public class

---
+++ src/datastructures/DoublyLinkedLists/DoublyLinkedList.java
@@ -0,0 +1,25 @@
+package datastructures.DoublyLinkedLists;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
