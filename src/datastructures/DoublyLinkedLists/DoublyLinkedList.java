d85153df2947e3dd175440e2389c089f8662970e
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-04 19:05:08 -0400

    added the basic  print head, tail and length methods

--- src/datastructures/DoublyLinkedLists/DoublyLinkedList.java
+++ src/datastructures/DoublyLinkedLists/DoublyLinkedList.java
@@ -22,4 +22,32 @@ public class DoublyLinkedList {
         tail = newNode;
         length = 1;
     }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
 }
---
+++ src/datastructures/DoublyLinkedLists/Main.java
@@ -0,0 +1,8 @@
+package datastructures.DoublyLinkedLists;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
