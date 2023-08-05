c9afff82ddf34ff2a8135266159e1c81644b20b6
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-05 13:57:47 -0400

    Added a get method

--- src/datastructures/DoublyLinkedLists/Main.java
+++ src/datastructures/DoublyLinkedLists/Main.java
@@ -3,20 +3,13 @@ package datastructures.DoublyLinkedLists;
 public class Main {
 
     public static void main(String[] args) {
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
-
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
-
-        myDLL.printList();
-
-        myDLL.append(8);
-
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
 
         myDLL.printList();
+
+        System.out.println(myDLL.get(3).value);
     }
 }
