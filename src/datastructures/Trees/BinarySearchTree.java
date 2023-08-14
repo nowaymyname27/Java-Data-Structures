711be1586d8c694ba80c9d8ce655fb29ed2bf9b7
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-14 19:11:15 -0400

    Added an insert method

--- src/datastructures/Trees/BinarySearchTree.java
+++ src/datastructures/Trees/BinarySearchTree.java
@@ -2,7 +2,7 @@ package datastructures.Trees;
 
 public class BinarySearchTree {
 
-    private Node root;
+    Node root;
 
     class Node {
         int value;
@@ -14,7 +14,30 @@ public class BinarySearchTree {
         }
     }
 
-    class void insert(){
-
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) {
+                return false;
+            }
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
     }
 }
