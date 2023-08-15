8bff3f8770c9815665e533618214ea89bea7e69a
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-15 19:39:04 -0400

    Added a Main Class to test HashTables

---
+++ src/datastructures/HashTables/Main.java
@@ -0,0 +1,10 @@
+package datastructures.HashTables;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.printTable();
+    }
+
+}
