c99e2654ffef785d3078bc6101527f77a22c42e3
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-15 21:07:05 -0400

    Added a set method

--- src/datastructures/HashTables/HashTable.java
+++ src/datastructures/HashTables/HashTable.java
@@ -39,4 +39,19 @@ public class HashTable {
         }
         return hash;
     }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
 }
