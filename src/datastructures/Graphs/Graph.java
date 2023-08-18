c558ae1c6a8945123fd4b834afd8a297eb80453d
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-17 21:30:00 -0400

    Adding a Graph class with a Main testing class

---
+++ src/datastructures/Graphs/Graph.java
@@ -0,0 +1,20 @@
+package datastructures.Graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) == null) {
+            adjList.put(vertex, new ArrayList<String>());
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/datastructures/Graphs/Main.java
@@ -0,0 +1,13 @@
+package datastructures.Graphs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+
+        myGraph.printGraph();
+    }
+
+}
