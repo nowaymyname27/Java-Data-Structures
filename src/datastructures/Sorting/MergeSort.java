e50bf91cbbf188df3dc9bc6156d579400728b057
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-23 17:13:44 -0400

    Adding MergeSort function

--- src/datastructures/Sorting/MergeSort.java
+++ src/datastructures/Sorting/MergeSort.java
@@ -33,10 +33,22 @@ public class MergeSort {
         return combined;
     }
 
+    public static int[] mergeSort(int[] array) {
+        if (array.length == 1)
+            return array;
+
+        int midIndex = array.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+        int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+
+        return merge(left, right);
+    }
+
     public static void main(String[] args) {
 
-        int[] array1 = { 1, 3, 7, 8 };
-        int[] array2 = { 2, 4, 5, 6 };
-        System.out.println(Arrays.toString(merge(array1, array2)));
+        int[] originalArray = { 3, 1, 4, 2 };
+        int[] sortedArray = mergeSort(originalArray);
+        System.out.println(Arrays.toString(originalArray));
+        System.out.println(Arrays.toString(sortedArray));
     }
 }
