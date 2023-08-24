22cba25d0b474954fbcc761bc8c5e01765dad1d4
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-24 16:24:01 -0400

    Adding QuickSort class staring with two helper functions

---
+++ src/datastructures/Sorting/QuickSort.java
@@ -0,0 +1,24 @@
+package datastructures.Sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    public static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+        return swapIndex;
+    }
+}
