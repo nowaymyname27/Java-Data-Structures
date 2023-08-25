ab0b762e38c530e92c3ec778740874c6363d4222
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-25 19:28:43 -0400

    Testing all tree traversal methods

--- src/datastructures/Trees/Main.java
+++ src/datastructures/Trees/Main.java
@@ -12,6 +12,9 @@ public class Main {
         myBST.insert(52);
         myBST.insert(82);
 
+        System.out.println(myBST.BFS());
+        System.out.println(myBST.DFSPreOrder());
+        System.out.println(myBST.DFSPostOrder());
         System.out.println(myBST.DFSInOrder());
     }
 }
