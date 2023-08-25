b6bf4700c9cc0e8b81c0b614ea683d8926573566
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-25 19:09:53 -0400

    Adding a DFS PostOrder method to BinarySearchTree

--- src/datastructures/Trees/Main.java
+++ src/datastructures/Trees/Main.java
@@ -12,6 +12,6 @@ public class Main {
         myBST.insert(52);
         myBST.insert(82);
 
-        System.out.println(myBST.DFSPreOrder());
+        System.out.println(myBST.DFSPostOrder());
     }
 }
